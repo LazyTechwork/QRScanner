152dbc0a2a762349d1a572a303dd9db23b39955c
LazyTechwork <lazytechwork@gmail.com>
2020-05-02 20:47:00 +0300

    Added fragments

--- app/src/main/java/ru/lazytechwork/qrscanner/MainActivity.kt
+++ app/src/main/java/ru/lazytechwork/qrscanner/MainActivity.kt
@@ -1,12 +1,37 @@
 package ru.lazytechwork.qrscanner
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import ru.lazytechwork.qrscanner.fragments.FavouritesFragment
+import ru.lazytechwork.qrscanner.fragments.HistoryFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navbar: BottomNavigationView = findViewById(R.id.navbar)
+        navbar.setOnNavigationItemSelectedListener(navbarListener)
     }
+
+    private val navbarListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { menuItem: MenuItem ->
+            when (menuItem.itemId) {
+                R.id.navitem_history -> {
+                    val fragment = HistoryFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.app_container, fragment, fragment.javaClass.simpleName)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navitem_favourites -> {
+                    val fragment = FavouritesFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.app_container, fragment, fragment.javaClass.simpleName)
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
 }
---
+++ app/src/main/java/ru/lazytechwork/qrscanner/fragments/FavouritesFragment.kt
@@ -0,0 +1,16 @@
+package ru.lazytechwork.qrscanner.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.lazytechwork.qrscanner.R
+class FavouritesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_favourites, container, false)
+    }
+}
