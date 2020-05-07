f8bec99e5ca19dffc511750036d0ddd0cb9026ba
LazyTechwork <lazytechwork@gmail.com>
2020-05-08 01:15:18 +0300

    Adding new scan to scanlist

--- app/src/main/java/ru/lazytechwork/qrscanner/fragments/ScannerFragment.kt
+++ app/src/main/java/ru/lazytechwork/qrscanner/fragments/ScannerFragment.kt
@@ -10,22 +10,40 @@ import com.budiyev.android.codescanner.CodeScanner
 import com.budiyev.android.codescanner.CodeScannerView
 import com.budiyev.android.codescanner.DecodeCallback
 import ru.lazytechwork.qrscanner.R
+import ru.lazytechwork.qrscanner.data.ScanType
+import ru.lazytechwork.qrscanner.data.cache.CacheMaster
+import ru.lazytechwork.qrscanner.sql.Scan
+import java.util.*
+import kotlin.random.Random
 
 class ScannerFragment : Fragment() {
     private lateinit var codeScanner: CodeScanner
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_scanner, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val scannerView = view.findViewById<CodeScannerView>(R.id.scanner_view)
         val activity = requireActivity()
+        scannerView
         codeScanner = CodeScanner(activity, scannerView)
         codeScanner.decodeCallback = DecodeCallback {
             activity.runOnUiThread {
-                Toast.makeText(activity, it.text, Toast.LENGTH_LONG).show()
+                CacheMaster.newScan(
+                    Scan(
+                        Random.nextInt(),
+                        "Новое сканирование",
+                        it.text,
+                        Date(),
+                        ScanType.TEXT,
+                        false
+                    )
+                )
+                Toast.makeText(activity, it.text, Toast.LENGTH_SHORT).show()
             }
         }
         scannerView.setOnClickListener {
